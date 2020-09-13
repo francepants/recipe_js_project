1d82d5b57795344c58d82fabe72e6cf91b3c444a
francepants <franceline90@gmail.com>
2020-09-13 09:16:31 -0500

    got recipes to show

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -1,1 +1,28 @@
-alert('it works')
+document.addEventListener('DOMContentLoaded', handleEvents())
+
+function handleEvents() {
+    loadRecipes()
+}
+
+function loadRecipes() {
+    fetch('http://localhost:3000/recipes')
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(recipes){
+        recipes.forEach(function(recipe){
+            displayRecipe(recipe)
+        })
+    })
+}
+
+function displayRecipe(recipe){
+    const main = document.querySelector('div#main')
+    const div = document.createElement('div')
+
+    div.innerHTML = `
+    <h3>${recipe.name}</h3>
+    <p>${recipe.description}</p>
+    `
+    main.appendChild(div)
+}

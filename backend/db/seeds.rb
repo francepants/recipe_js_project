5f51bda9780d4aee7e74ec14821edea0f1c701ac
francepants <franceline90@gmail.com>
2020-09-13 08:56:05 -0500

    ran migration, added notes, seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+recipes = Recipe.create([
+    { name: 'Sofrito', description: 'Sofrito is the base for most Puerto Rican dishes, and this one is better than store bought (difficult to find in the stores in the Western United States). This can be added to beans, rice, soups, stews, you name it.'}, 
+    { name: 'Arroz con Gandules', description: "There’s nothing more quintessentially Puerto Rican than arroz con gandules.  It’s part of our national dish (along with Pernil).  For holidays, birthdays, baby showers, and more, if there’s a party, this dish will be there.  Everyone has a little twist on how they prepare this dish, but here’s my version."}
+    ])

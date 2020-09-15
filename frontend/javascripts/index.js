0c9feb13ec8e45824c2f0300586f92e7f7f14bc7
francepants <franceline90@gmail.com>
2020-09-14 22:40:04 -0500

    recipe name and description inputs made. need to work on getting them to work

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -2,6 +2,7 @@ const BASE_URL = "http://localhost:3000"
 const RECIPES_URL = "http://localhost:3000/recipes"
 const INGREDIENTS_URL = "http://localhost:3000/ingredients"
 const main = document.querySelector('div#main')
+const br = document.createElement('br')
 
 document.addEventListener('DOMContentLoaded', handleEvents())
 
@@ -80,26 +81,33 @@ function displayIngredients(ingredients){
 // create recipe form
 function createRecipeForm() {
     const recipeForm = document.createElement('form')
-    recipeForm.setAttribute('method',"post")
-    recipeForm.setAttribute('action',"submit")
+    // recipeForm.setAttribute('method',"post")
+    // recipeForm.setAttribute('action',"submit")
+    recipeForm.id = "recipe-form"
+
 
     const recipeDiv = document.createElement('div')
 
-    const recipeInput = document.createElement("input") // input element/text
-    recipeInput.setAttribute('type',"text")
-    recipeInput.setAttribute('name',"recipe-name")
+    const recipeNameInput = document.createElement("input") // input element/text
+    recipeNameInput.setAttribute('type',"text")
+    recipeNameInput.setAttribute('name',"recipe-name")
+    recipeNameInput.id = "recipe-name"
+    recipeNameInput.placeholder = "Recipe Name"
+    
+    const recipeDescriptionInput = document.createElement("input") // input element/text
+    recipeDescriptionInput.setAttribute('type',"text")
+    recipeDescriptionInput.setAttribute('name',"recipe-name")
+    recipeDescriptionInput.id = "recipe-description"
+    recipeDescriptionInput.placeholder = "Recipe Description"
 
-    const recipeSubmitButton = document.createElement("input")
+    const recipeSubmitButton = document.createElement("input") // submit button
     recipeSubmitButton.setAttribute('type',"submit")
     recipeSubmitButton.setAttribute('value',"Create Recipe")
 
     recipeForm.appendChild(recipeDiv)
-    recipeForm.appendChild(recipeInput)
+    recipeForm.appendChild(recipeNameInput)
+    recipeForm.appendChild(recipeDescriptionInput)
     recipeForm.appendChild(recipeSubmitButton)
 
     document.getElementById('main').appendChild(recipeForm);
 }
-// add recipe button
-
-
-//random

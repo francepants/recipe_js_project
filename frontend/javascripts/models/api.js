56fa8801fd54e0f2b7f7747dd0014cc81cbf9bcc
francepants <franceline90@gmail.com>
2020-09-20 16:02:51 -0500

    moved load ingredients to api.js -nothing broken

--- frontend/javascripts/models/api.js
+++ frontend/javascripts/models/api.js
@@ -5,11 +5,20 @@ class API {
         .then(resp => resp.json())
         .then(recipes => recipes.forEach(recipe => displayRecipe(recipe)))
     }
-
-    // EDIT recipe
     
-
-    // ingredients
-
+    // load ingredients
+    static loadIngredients(e) {
+        let parentNode = this.parentNode
+        // http://localhost:3000/recipes/1
+        fetch(`http://localhost:3000/recipes/${this.id}`)
+        .then(resp => resp.json())
+        .then(function(recipe){
+            let ingredientForRecipeDiv = parentNode.querySelector('#ingredient-for-recipe-div')
+            ingredientForRecipeDiv.innerHTML = ""
+            recipe.ingredients.forEach(function(ing){
+                displayIngredient(parentNode, ing)
+            })
+        })
+    }
 
 }

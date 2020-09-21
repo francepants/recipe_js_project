033bdc0f0ceecc2403ad0b8a11cb485a0a01943a
francepants <franceline90@gmail.com>
2020-09-20 21:35:15 -0500

    organizing code

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -1,18 +1,20 @@
 const BASE_URL = "http://localhost:3000"
 const RECIPES_URL = "http://localhost:3000/recipes"
 const INGREDIENTS_URL = "http://localhost:3000/ingredients"
-const main = document.querySelector('div#main')
-const recipeSubmitButton = () => document.getElementById('submit-recipe')
 
+const main = document.querySelector('div#main')
+
+//recipe
 let editing = false
 let editedRecipeId = null
 let currentRecipeId = null
-
 const form = () => document.getElementById('recipe-form')
+const recipeCookTime = () => document.getElementById('recipe-cook-time')
 const recipeName = () => document.getElementById('recipe-name')
 const recipeDescription = () => document.getElementById('recipe-description')
-const recipeCookTime = () => document.getElementById('recipe-cook-time')
+const recipeSubmitButton = () => document.getElementById('submit-recipe')
 
+//ingredient
 const ingredientName = () => document.getElementById('ingredient-name')
 const ingredientMeasurement = () => document.getElementById('ingredient-measurement')
 
